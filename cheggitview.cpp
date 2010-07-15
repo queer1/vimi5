e5bc6bd047ce504ef7d61e56079b28514c2477b8
cwk <coolwk@gmail.com>
2010-07-15 04:16:16 +0200

    fix parsing, using regexps

--- cheggitview.cpp
+++ cheggitview.cpp
@@ -2,7 +2,7 @@
 #include <QDebug>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QDomDocument>
+#include <QRegExp>
 #include <QPair>
 
 
@@ -36,7 +36,8 @@ CheggitView::CheggitView(QWidget *parent) :
 
 void CheggitView::search()
 {
-    searchMovies("cumshots");
+    //searchMovies("cumshots");
+    getTags(QUrl("http://cheggit.net/torrents.php?torrentid=112758"));
 }
 
 void CheggitView::searchMovies(const QString &title)
@@ -58,34 +59,27 @@ void CheggitView::parseSearch(QNetworkReply *reply)
 
     disconnect(this, SLOT(parseSearch(QNetworkReply*)));
     if (!reply->error()) {
-        QList<QPair<QString, QUrl> > urls;
-        QByteArray request = reply->readAll();
+        QString request = QString(reply->readAll());
         if (request.isEmpty()) {
             qWarning() << "Read no data from cheggit!";
             return;
         }
-        qDebug() << request;
-        QString error;
-        QDomDocument doc("search");
-        if (!doc.setContent(request, false, &error)) {
-            qWarning() << error;
-        }
 
+        QRegExp regexp("(torrents.php\\?torrentid=[0-9]+)\">([^<]+)");
+        QList<QPair<QString, QUrl> > urls;
         QString urlBeginning("http://cheggit.net/");
         QString url;
         QString title;
 
-        QDomNode node = doc.documentElement().firstChild();
-        while (!node.isNull()) {
-            QDomElement element = node.toElement();
-            qDebug() << element.tagName() << element.attribute("href");
-            if (!element.isNull() && element.tagName() == "a" && element.attribute("href").startsWith("torrents.php?torrentid=")) {
-                url = element.attribute("href");
-                title = element.text();
-                qDebug() << title << url;
-                urls.append(QPair<QString, QUrl>(title, QUrl(urlBeginning + url)));
-            }
-            node = node.nextSibling();
+        int pos = 0;
+
+        while ((pos = regexp.indexIn(request, pos)) != -1) {
+            url = regexp.cap(1);
+            title = regexp.cap(2);
+            pos += regexp.matchedLength();
+            if (url.isEmpty() || title.isEmpty()) continue;
+            qDebug() << title << url;
+            urls.append(QPair<QString, QUrl>(title, QUrl(urlBeginning + url)));
         }
         if (!urls.isEmpty())
             emit foundMovies(urls);
@@ -94,9 +88,37 @@ void CheggitView::parseSearch(QNetworkReply *reply)
 
 void CheggitView::getTags(const QUrl &movie)
 {
+    connect(m_movieAccessManager, SIGNAL(finished(QNetworkReply*)), SLOT(parseMovie(QNetworkReply*)));
+    m_movieAccessManager->get(QNetworkRequest(movie));
 }
 
 void CheggitView::parseMovie(QNetworkReply *reply)
 {
+    qDebug() << "Got movie reply";
 
+    disconnect(this, SLOT(parseSearch(QNetworkReply*)));
+    if (!reply->error()) {
+        QString request = QString(reply->readAll());
+        if (request.isEmpty()) {
+            qWarning() << "Read no data from cheggit!";
+            return;
+        }
+
+        QRegExp regexp("tags\\.php\\?filter=([^\"]+)");
+        QStringList tags;
+        QString tag;
+
+        int pos = 0;
+
+        while ((pos = regexp.indexIn(request, pos)) != -1) {
+            tag = regexp.cap(1);
+            pos += regexp.matchedLength();
+            if (tag.isEmpty()) continue;
+            qDebug() << tag;
+            tags << tag;
+        }
+
+        if (!tags.isEmpty())
+            emit foundTags(tags);
+    }
 }
